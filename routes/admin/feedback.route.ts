a7595f24c5a237103d30b93027770d596c231ead
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-03-29 12:04:29 +0700

    feat(admin): [CPC-8] implement view details and update feedback status

--- routes/admin/feedback.route.ts
+++ routes/admin/feedback.route.ts
@@ -1,5 +1,11 @@
 import express, { Router } from "express";
 const router: Router = Router()
 import * as controller from "../../controllers/admin/feedback.controller"
-router.get("/",controller.index)
+router.get("/", controller.index)
+
+router.get("/detail/:id", controller.detail)
+
+router.patch("/change-status/:status/:id", controller.changeStatus)
+router.patch("/change-multi", controller.changeMulti)
+
 export const feedbackRoutes: Router = router
